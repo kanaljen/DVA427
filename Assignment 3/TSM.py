6a5148c9d5c2467663bd4411f781974f9066bf0d
PeterAndersson <peteranderssonkolback@gmail.com>
2018-02-06 17:27:40 +0100

    crossover function not working

--- Assignment 3/TSM.py
+++ Assignment 3/TSM.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class OverHead(object):
 
 	def __init__(self, n_cities=52, n_salesmen=10):
@@ -5,6 +7,8 @@ class OverHead(object):
 		self.city_matrix = np.empty(shape=(n_cities, n_cities)) # Matrix to define the distance between all cities
 		self.city = []
 		self.salesman = []
+		self.offspring = []
+		self.best_salesmen = []
 
 		for i in range(n_cities):
 			self.city.append(City()) # Vector of the cities
@@ -12,27 +16,94 @@ class OverHead(object):
 		for i in range(n_salesmen):
 			self.salesman.append(Salesman(n_cities))
 
+		for i in range(n_salesmen-2):
+			self.offspring.append(Salesman(n_cities))
+
+	def distance_two_cities(self,i,k):
+		#Returns the distance between two cities a and b
+		distance = np.sqrt(np.square(self.city[k].x-self.city[i].x)+np.square(self.city[k].y-self.city[i].y)) 
+		return distance
+
+	def fitness_evaluator(self):
+		# Returns a vector with the indexes of the best salesmen. Best salesman on index 0
+		#self.best_salesmen = np.empty(shape=(1, 1),dtype=int)
+		self.best_salesmen = np.array([0])
+		#self.best_salesmen[0] = 0
+		for i in range(1,len(self.salesman)):
+			for k in range(len(self.salesman)):
+				if k >= i:
+					self.best_salesmen = np.insert(self.best_salesmen,k,i)
+					break
+				elif self.salesman[i].tot_dist < self.salesman[self.best_salesmen[k]].tot_dist:
+					self.best_salesmen = np.insert(self.best_salesmen,k,i)
+					break
+
+
 	def add_cities(self, c_set):
 		# Adding all cities from c_set
-		for i in range(c_set[0]):
+		for i in range(len(c_set[:,0])):
 			# Loop through all cities
-			self.city[i] = c_set[i]
+			self.city[i].x = c_set[i,0]
+			self.city[i].y = c_set[i,1]
 			self.city_matrix[i,i] = 0
 			for k in range(i):
 				# Define the distance between all cities				
-				self.city_matrix[i,k] = distance_two_cities(self.city[i],self.city[k])
+				self.city_matrix[i,k] = self.distance_two_cities(i,k)
 				self.city_matrix[k,i] = self.city_matrix[i,k] # Matrix is symmetric
-
-	def distance_two_cities(a,b):
-		#Returns the distance between two cities a and b
-		distance = np.sqrt(np.square(b.x-a.x)+np.square(b.y-a.y)) 
-		return distance
 
 	def create_salesmen(self):
 
-		for i in range(len(self.salesman[:,0])):
-			for k in range(len(self.route)):
+		for i in range(len(self.salesman)):
+			for k in range(len(self.salesman[i].route)):
 				self.salesman[i].tot_dist = self.salesman[i].tot_dist + self.city_matrix[self.salesman[i].route[k-1],self.salesman[i].route[k]]
+
+	def crossover(self,a,b):
+
+		self.salesman[0].route = self.salesman[a].route
+		self.salesman[1].route = self.salesman[b].route
+		#print('salsesman[0]route len=',len(self.salesman[0].route))
+
+		sequence_length = 10
+
+		for i in range(len(self.offspring)):
+			self.offspring[i] = self.salesman[np.remainder(i,2)] # Copies a route from parent 1
+			sequence_start = np.random.randint(1,len(self.salesman[i].route)-sequence_length)	
+			#print('len salesman[i].route=',len(self.salesman[i].route))
+			#print('sequence start=',sequence_start)		
+			sequence_array = np.empty(shape=(sequence_length,1))
+			index_delete = np.empty(shape=(sequence_length,1))
+			index = 0
+			for j in range(sequence_length):
+				# Saves a sequence from parent 2 and stores it in a temporary vector
+				sequence_array[j] = self.salesman[np.remainder(i+1,2)].route[sequence_start+j]
+			for j in range(len(self.salesman[i].route)):
+				# Checks which cities in the route that is also in the sequence from parent 2
+				if np.any(sequence_array==self.offspring[i].route[j]):
+					index_delete[index] = j
+					index+=1
+			# Delete the duplicate cities before adding them
+			#print('delete_index=',index_delete)
+			#print('före delete',self.offspring[i].route)
+			self.offspring[i].route = np.delete(self.offspring[i].route,index_delete)
+			#print('efter delete',self.offspring[i].route)
+			for k in range(sequence_length):
+				# Add the sequence from parent 2 to the offspring
+				self.offspring[i].route = np.insert(self.offspring[i].route,sequence_start+k,sequence_array[k])
+
+		for i in range(len(self.offspring)):
+			self.salesman[i+2] = self.offspring[i]
+
+	def test(self,c_set):
+		self.add_cities(c_set)
+		self.create_salesmen()
+
+		for i in range(10):
+			self.fitness_evaluator()
+			self.crossover(self.best_salesmen[0],self.best_salesmen[1])
+			#print(self.best_salesmen[0])
+			print(self.salesman[self.best_salesmen[0]].tot_dist)
+
+
 
 class City(object):
 
--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,9 +1,42 @@
 import numpy as np
+import TSM
+
+infile = open('berlin52.tsp', 'r')
+
+# Read instance header
+Name = infile.readline().strip().split()[1] # NAME
+FileType = infile.readline().strip().split()[1] # TYPE
+Comment = infile.readline().strip().split()[1] # COMMENT
+Dimension = infile.readline().strip().split()[1] # DIMENSION
+EdgeWeightType = infile.readline().strip().split()[1] # EDGE_WEIGHT_TYPE
+infile.readline()
+
+# Read node list
+nodelist = []
+N = int(Dimension)
+for i in range(0, int(Dimension)):
+    x,y = infile.readline().strip().split()[1:]
+    nodelist.append([float(x), float(y)])
+
+# Close input file
+infile.close()
+dataset = np.array(nodelist)
+
+TSM = TSM.OverHead(n_cities=52, n_salesmen=10)
+TSM.test(dataset)
+
+"""TSM.add_cities(dataset)
+TSM.create_salesmen()
+
+print(len(TSM.salesman[0].route))
+
+for i in range(len(TSM.salesman)):
+	print(TSM.salesman[i].tot_dist)
+
+TSM.fitness_evaluator()
+
+print(TSM.best_salesmen)"""
 
 
-x = np.arange(1,52)
-x = np.append(x,0)
-x = np.insert(x,0,0)
-print(x)
-for i in range(len(x)):
-	print(x[i-1])
+
+
